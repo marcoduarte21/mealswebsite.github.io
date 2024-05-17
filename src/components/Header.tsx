9d373191acfaabc273aefd7efbd3b7f85e300dd8
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-05-16 18:28:38 -0600

    added navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,46 @@ interface HeaderProps{
 
 export const Header: React.FC<HeaderProps> = ({}) =>{
 
+    function handleClick(){
+        const sidebar = document.querySelector('.sidebar');
+        if (sidebar instanceof HTMLElement) {
+        sidebar.style.display = 'flex';
+        } else {
+    console.error('Elemento .sidebar no encontrado o no es un HTMLElement');
+            }
+    }
+
+    function hiddenSideBar(){
+        const sidebar = document.querySelector('.sidebar');
+        if (sidebar instanceof HTMLElement) {
+        sidebar.style.display = 'none';
+        } else {
+    console.error('Elemento .sidebar no encontrado o no es un HTMLElement');
+            }
+    }
+
     return (
+        <>
         <header>
-            <a href="#" id="bars"><i className="fa-solid fa-bars"></i></a>
-            <nav>
+            <button id="bars" onClick={handleClick}><i className="fa-solid fa-bars"></i></button>
+            <nav id="navBar">
                 <ul>
-                    <li><Link to={"#"}>Categories</Link></li>
-                    <li><Link to={"#"}>Areas</Link></li>
-                    <li><Link to={"#"}>Ingredients</Link></li>
+                <li><Link to={"#"}>Categories</Link></li>
+                <li><Link to={"#"}>Areas</Link></li>
+                <li><Link to={"#"}>Ingredients</Link></li>
                 </ul>
             </nav>
             <h1>allrecipes<i className="fa-solid fa-spoon"></i></h1>
             <a href="#"><i className="fa-solid fa-magnifying-glass"></i></a>
-    </header>
+        </header>
+            <nav >
+                <ul className="sidebar">
+                <li id="hidenSideBar" onClick={hiddenSideBar}><i className="fa-solid fa-xmark"></i></li>
+                <li><Link to={"#"}>Categories</Link></li>
+                <li><Link to={"#"}>Areas</Link></li>
+                <li><Link to={"#"}>Ingredients</Link></li>
+                </ul>
+            </nav>
+    </>
     );
 }
