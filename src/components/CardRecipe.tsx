dd62672a29cc5ebc77d8d0724b7a38ce3ab9f900
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-05-16 22:34:29 -0600

    added functionality button star

--- src/components/CardRecipe.tsx
+++ src/components/CardRecipe.tsx
@@ -8,14 +8,24 @@ interface CardRecipeProps{
 
 export const CardRecipe: React.FC<CardRecipeProps> = ({img, alt, title}) =>{
 
+    function handleClick(e:any){
+
+
+            if (e.target.classList.contains('fa-regular')) {
+                e.target.classList.remove('fa-regular');
+                e.target.classList.add('fa-solid');
+            } else {
+                e.target.classList.remove('fa-solid');
+                e.target.classList.add('fa-regular');
+            }
+    }
+
     return(
                 <Link to={"#"}>
                 <img src={img} alt={alt} />
             <div className="description">
                 <h3>{title}</h3>
-                <div className="stars">
-                <button className="favorite"><i className="fa-regular fa-star"></i></button>
-            </div>
+                <i onClick={handleClick} id="starIcon" className="fa-regular fa-star"></i>
             </div>
                 </Link>
     )
